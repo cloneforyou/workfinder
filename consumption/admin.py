d9ab5efa7f446fb989190a3228e0c04b9412979a
Tony Truong <tony888t@googlemail.com>
2019-12-08 02:22:06 +0000

    Added admin. Updated .gitignore.

--- consumption/admin.py
+++ consumption/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Hotel, Meter, HalfHourly
+
+
+@admin.register(Hotel)
+class HotelAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Meter)
+class MeterAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(HalfHourly)
+class HalfHourlyAdmin(admin.ModelAdmin):
+    pass
